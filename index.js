b75c2f0af80d296268dc48cbd3529c3997a9a2ae
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2025-02-19 15:56:56 +0530

    implement Pawn class and getPossibleMoves function for move generation

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class ChessBoard {
     this.rows = "12345678";
     this.col = position[0];
     this.row = parseInt(position[1]);
-    this.colIndex = this.columns.indexOf(col);
+    this.colIndex = this.columns.indexOf(this.col);
   }
   /**
    * Checks if the move is valid on a chessboard.
@@ -41,6 +41,25 @@ class ChessBoard {
   }
 }
 
-function getPossibleMoves(piece, position) {}
-
+class Pawn extends ChessBoard {
+  getPossibleMoves() {
+    return this.row < 8 ? [`${this.col}${this.row + 1}`] : [];
+  }
+}
+/**
+ * Get the possible moves for a given chess piece at a specific position.
+ *
+ * @param {string} piece - The type of chess piece (e.g., "Pawn").
+ * @param {string} position - The current position of the piece on the chessboard (e.g., "G1").
+ * @returns {string} A comma-separated string of possible moves or an error message.
+ */
+function getPossibleMoves(piece, position) {
+  const pieces = {
+    pawn: new Pawn(position),
+  };
+  if (!pieces[piece.toLowerCase()]) return "Invalid piece";
+  const moves = pieces[piece.toLowerCase()].getPossibleMoves();
+  return moves.length > 0 ? moves.join(", ") : "Move not possible";
+}
+// getPossibleMoves("Pawn", "G1");
 module.exports = { getPossibleMoves };
